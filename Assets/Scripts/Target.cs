bf84babb0ebf8ca7c7b0336d19f1334e6c61679e
qusitu1 <122053882+qusitu1@users.noreply.github.com>
2023-03-21 09:40:34 -0700

    End of lesson 5.2 step 3

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -19,7 +19,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
-        transform.position = RandomSpawnPos;
+        transform.position = RandomSpawnPos();
     }
 
     // Update is called once per frame
@@ -27,6 +27,17 @@ public class Target : MonoBehaviour
     {
         
     }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
 
     Vector3 RandomForce()
     {
